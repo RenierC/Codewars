cf1b6b38ff9359bb730a014471a96f561224e9c2
RenierC <reniercuervo@gmail.com>
2020-06-19 21:16:26 -0400

    done

---
+++ Take a Ten Minute Walk.js
@@ -0,0 +1,30 @@
+function isValidWalk(walk) {
+  let n = 0;
+  let s = 0;
+  let w = 0;
+  let e = 0;
+
+  if (walk.length === 10) {
+    for (let direction of walk) {
+      switch (direction) {
+        case "n":
+          n++;
+          break;
+        case "s":
+          s++;
+          break;
+        case "e":
+          e++;
+          break;
+        case "w":
+          w++;
+          break;
+      }
+    }
+
+    if (n === s && w === e) {
+      return true;
+    }
+    return false;
+  }
+}
