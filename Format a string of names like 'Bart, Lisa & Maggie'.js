f353a7bb654a781a0777a3a8b3c719d0ce62b42d
RenierC <reniercuervo@gmail.com>
2020-06-21 23:59:47 -0400

    finish

---
+++ Format a string of names like 'Bart, Lisa & Maggie'.js
@@ -0,0 +1,29 @@
+function list(names) {
+  let arr = [];
+  let chars = "";
+
+  // I convert the hashes into an array
+
+  for (let count of names) {
+    arr.push(count.name);
+  }
+  // loop the array
+  for (let i = 0; i < arr.length; i++) {
+    let last = arr.length - 1;
+    let secondLast = last - 1;
+
+    // check to see if i'm in the last to put nothing
+    // second last to add &
+    // none of those to add comma ,
+    // after that I add them to the chars variable aggregating in each iteration
+
+    if (i === last) {
+      chars += arr[i];
+    } else if (i === secondLast) {
+      chars += arr[i] + " & ";
+    } else {
+      chars += arr[i] + ", ";
+    }
+  }
+  return chars;
+}
