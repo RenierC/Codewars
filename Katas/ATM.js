5f7b0a6dde92270108032e557ff92c5207a79f69
RenierC <reniercuervo@gmail.com>
2020-07-09 12:02:01 -0400

    🏧

---
+++ Katas/ATM.js
@@ -0,0 +1,14 @@
+function solve(n) {
+  let notes = [500, 200, 100, 50, 20, 10];
+  let total = n;
+  let result = [];
+
+  for (let i = 0; i < notes.length; i++) {
+    result.push(Math.floor(total / notes[i]));
+    total = total % notes[i];
+  }
+
+  let bills = result.reduce((a, b) => a + b);
+
+  return total !== 0 ? -1 : bills;
+}
