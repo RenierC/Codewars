09d325e379934b9ff2d04606a74251880e6fbf93
RenierC <reniercuervo@gmail.com>
2020-12-08 10:50:05 -0400

    👆🏽 👇🏽

---
+++ Katas/Sum without highest and lowest number.js
@@ -0,0 +1,8 @@
+function sumArray(array) {
+  return array === null || array.length <= 2
+    ? 0
+    : array
+        .sort((a, b) => a - b)
+        .slice(1, -1)
+        .reduce((a, b) => a + b);
+}
