71c8e4331cce9a3475ac68142b88d2639e386e0f
Renier Cuervo <reniercuervo@gmail.com>
2020-12-25 21:06:33 -0400

    🐠

--- Katas/Find the unique number.js
+++ Katas/Find the unique number.js
@@ -2,6 +2,6 @@ function findUniq(arr) {
   let op = arr.reduce((total, num) => {
     num in total ? total[num]++ : (total[num] = 1);
     return total;
-  }, {});
+  },{});
   return Number(Object.keys(op).find((key) => op[key] === 1));
 }
